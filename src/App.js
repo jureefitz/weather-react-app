739f7f1ec36bcfb4e6cb0decef82527107d70c9d
jureefitz <42006462+jureefitz@users.noreply.github.com>
2023-01-07 02:05:05 -0500

    Added background image, updated css classes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="conatiner">
+      <div className="container">
         <div className="appborder">
          <Weather defaultCity="Cleveland"/>
         
